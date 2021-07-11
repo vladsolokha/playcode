9ecab9d59365c4cbd335f17824f43afc3cb43fb3
Vlad Solokha <vsolokha@gmail.com>
2021-07-11 00:13:36 -0600

    added arithmetic problem from challenge unknown.

---
+++ arithmetic-v2-working.py
@@ -0,0 +1,83 @@
+def arithmetic_arranger(problems, *calculate):
+  # Count no. of problems.
+  numofproblems = len(problems)
+  
+  # Check if there are more than 5 problems.
+  if numofproblems > 5:
+    print("Error: Too Many Problems")
+  
+  # Make empty list to store problems to print later.
+  listofproblems = []
+  
+  # Loop through problems list with iterable problem. 
+  for problem in problems:
+    # Split into list of elements.
+    #print(problem)
+    splitproblem = problem.split(" ") #need to check for errors
+    #print(splitproblem)
+    listofproblems.append(splitproblem)
+    
+  
+    # Map elements to vars. 
+    firstnum = splitproblem[0]
+    secondnum = splitproblem[2]
+    op = splitproblem[1]
+    
+    # Check if numbers are digits.
+    if not firstnum.isdigit() and not secondnum.isdigit():
+      print("Error: Numbers must only contain digits")
+    
+    # Check if op (operator) is + or -.
+    if op != '+' and op != '-':
+      print("Error: Operator must be '+' or '-'")
+    
+    # Check if numbers are more than 4 digits
+    if len(firstnum) > 4 or len(secondnum) > 4:
+      print("Error: Numbers cannot be more than four digits")
+
+    # Evaluate if input option is true.
+    if calculate == True:
+      sumofprob = eval(problem)
+      splitproblem.append(str(sumofprob))
+    
+  # I have array of problems.
+  # Access each element and format to print.
+
+  #print(listofproblems)
+  for element in listofproblems:
+    firstnum = element[0]
+    secondnum = element[2]
+    totalproblength = len(max(firstnum, secondnum))+2
+    firstline =(" "*(totalproblength-len(firstnum))+firstnum)    
+    print(firstline, end="    ")
+
+  print()
+  for element in listofproblems:  
+    firstnum = element[0]
+    op = element[1]
+    secondnum = element[2]
+    totalproblength = len(max(firstnum, secondnum))
+    secondline =(op + " " + " "*(totalproblength-len(secondnum))+secondnum)
+    print(secondline, end="    ")
+    
+  print()
+  for element in listofproblems:  
+    firstnum = element[0]
+    secondnum = element[2]
+    totalproblength = len(max(firstnum, secondnum))+2
+    thirdline = ('-'*totalproblength)
+    print(thirdline, end="    ")
+    
+
+      
+
+   
+    
+
+#  for listofproblems in zip(listofproblems
+#    print(listofproblems)
+  
+
+  #return arranged_problems
+
+arithmetic_arranger(["32 + 698", "3801 - 2"], True)
--- hikerValleys.py
+++ hikerValleys.py
@@ -1,4 +1,4 @@
-# Given an array of letters, U = up and D = down
+# Given a string of letters representing path, U = up and D = down
 # A hiker is taking steps for each letter
 # Starts at sea level and ends at sea level
 # Return an int that determines how many valleys he went down into below sea level
@@ -6,12 +6,18 @@
 # If he goes below sea level, he's in a valley = 1, if he never returns to sea level valley is still = 1
 # If he goes above sea level, its a mountain, and then goes to a valley below sea level into valley, valley = 2
 
+# have int represent + if above sea level and - below sea level called height or h
+# += if up and -= if down
+# we need to count the number of times number became -
 def hikerValleys(steps, path):
+    h = 0
     valley = 0
+    for i in range(steps):
+        
     return valley 
 
 
-path = [U,D,D,D,D,U,U,U,D,D,U,U,U,U,U,U,D,D,D,D,D,D,U,D,D,U,D,U,U,D,U,U,U,U,D,D,U,U,D]
+path = 'UDDDUDUU'
 walk = len(path)
 
 print(hikerValleys(walk, path))
