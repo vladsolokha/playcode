f04c73945758adadd8ca38cc942522000e5ce64d
Vlad Solokha <vsolokha@gmail.com>
2021-07-31 15:07:29 -0600

    solved diagonal sum difference puzzle

---
+++ diag_sum_diff.py
@@ -0,0 +1,32 @@
+'''
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix arr is shown below:
+
+1 2 20
+5 3 1
+-9 8 54
+
+returns absolute| ( 1+3+54 ) - ( 20+3-9 ) |
+'''
+
+def diagonalDifference(arr):
+    p_diag = []
+    s_diag = []
+    p_pos = 0
+    s_pos = len(arr)-1
+    for i in range(len(arr)):
+        p_diag.append(arr[i][p_pos])
+        s_diag.append(arr[i][s_pos])
+        p_pos += 1
+        s_pos -= 1
+    
+    return abs(sum(p_diag)-sum(s_diag))
+        
+
+matrix = [
+    [15, 5, 9],
+    [3, 11, 7],
+    [8, -3, 4]
+]
+print(diagonalDifference(matrix))
