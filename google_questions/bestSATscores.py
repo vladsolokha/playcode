78fa363ff2aa875ad5e05afa9d7fc6209165bb4e
vladsolokha <vsolokha@gmail.com>
2022-05-23 15:56:02 -0600

    added google interview question from 5/23

---
+++ google_questions/bestSATscores.py
@@ -0,0 +1,28 @@
+'''
+Given CSV file with 1000s of following:
+
+Name, Company, SAT score
+vlad, DSST, 800
+sonny, Google, 1000
+bob, Google, 200
+brian, DSST, 300
+patty, IBM, 1400
+jerry, IBM, 500
+
+output: the company that has the highest average score
+
+ignore names
+for each company, record count of co and add to score for that co
+in dictionary
+d = {DSST: [800, 300], Google: [1000, 200], IBM: [1400, 500]}
+sum = 0
+for key,val in enumerate(d.items):
+    sum += val
+
+return key of max value in company
+'''
+
+def bestAvgSat(objData):
+    # record data in dict
+    companies = {}
+    
